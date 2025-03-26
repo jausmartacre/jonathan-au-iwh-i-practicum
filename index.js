e896d8dfeadb3894ea0134a8102aba440adc3a47
jausmartacre <36274717+jausmartacre@users.noreply.github.com>
2025-03-26 14:33:09 -0400

    Update
    - api key
    - route 1
    - homepage.pug setup
    - updates.pug

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,19 +9,55 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_API_KEY;
+
+
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
+
 // * Code for Route 1 goes here
+
+app.get('/', async (req, res) => {
+	const pets = 'https://api.hubspot.com/crm/v3/objects/pets?properties=name&properties=breed&properties=age';
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };    
+	try {
+        const resp = await axios.get(pets, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Pets | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
 
+app.get('/updates-cobj', (req, res) => {
+  res.render('updates', { title: 'Create New Pet' });
+});
+
+
+
+
+
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+
+
+
+
+
+
+
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
